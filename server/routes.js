5bafa4ad00b22455d708d9ef7647e74854dfb4e7
Abinash <abinashsamal18@gmail.com>
2023-04-23 01:42:35 +0530

    completed login and register

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("./models/userSchema");
 const bodyParser = require("body-parser");
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
@@ -28,15 +28,15 @@ router.post("/register", async (req, res, next) => {
       skill4,
       address,
     } = req.body;
-    // const usernameCheck = await User.findOne({ name });
-    // if (usernameCheck) {
-    //   return res.json({ msg: "Username already used", status: false });
-    // }
-    // const emailCheck = await User.findOne({ email });
-    // if (emailCheck) {
-    //   return res.json({ msg: "Email already used", status: false });
-    //   console.log("email present");
-    // }
+    const usernameCheck = await User.findOne({ name });
+    if (id) {
+      return res.json({ msg: "ID already used", status: false });
+    }
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
+      console.log("email present");
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       email,
@@ -55,7 +55,32 @@ router.post("/register", async (req, res, next) => {
     });
     delete user.password;
     console.log(req.body);
+    res.redirect("http://192.168.43.95:5501/public/login.html");
     return res.json({ status: true, user });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { id, password } = req.body;
+    const idCheck = await User.findOne({ id });
+    if (!idCheck) {
+      return res.json({ msg: "incorrect username or password", status: false });
+    }
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      idCheck.password
+    );
+    if (!isPasswordValid)
+      return res.json({ msg: "incorrect username or password", status: false });
+    delete idCheck.password;
+    // console.log(usernameCheck);
+    
+    // res.json({ status: true, user: idCheck });
+    res.redirect('http://192.168.43.95:5501/public/index.html')
+    return ;
   } catch (error) {
     console.log(error);
   }
